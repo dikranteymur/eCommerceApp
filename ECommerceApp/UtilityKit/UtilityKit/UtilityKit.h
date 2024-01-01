05c72ca21bda145213d2619c2858b4db76814b3f
dikranteymur <mdteymur@gmail.com>
2024-01-01 20:30:09 +0300

    Fixed pod files and integration

---
+++ ECommerceApp/UtilityKit/UtilityKit/UtilityKit.h
@@ -0,0 +1,18 @@
+//
+//  UtilityKit.h
+//  UtilityKit
+//
+//  Created by Dikran Teymur on 1.01.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UtilityKit.
+FOUNDATION_EXPORT double UtilityKitVersionNumber;
+
+//! Project version string for UtilityKit.
+FOUNDATION_EXPORT const unsigned char UtilityKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UtilityKit/PublicHeader.h>
+
+
