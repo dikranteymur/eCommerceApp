d26ad6ce1581079eb6d58b8d475d868b2ab45d9e
dikranteymur <mdteymur@gmail.com>
2024-01-02 03:25:38 +0300

    Added starting app with ProductListViewController

--- ECommerceApp/ECommerceApp/App/AppDelegate.swift
+++ ECommerceApp/ECommerceApp/App/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        app.router.startWith(viewController: UIViewController())
+        app.router.startWith(viewController: ProductListBuilder.make())
         return true
     }
 }
