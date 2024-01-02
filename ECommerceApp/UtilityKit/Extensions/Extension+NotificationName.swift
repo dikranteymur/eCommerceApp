73f7ead5f8237de8358cf7fc0455d41907835f43
dikranteymur <mdteymur@gmail.com>
2024-01-02 03:24:38 +0300

    Add notification center extensions for bag view total label

---
+++ ECommerceApp/UtilityKit/Extensions/Extension+NotificationName.swift
@@ -0,0 +1,15 @@
+//
+//  Extension+NotificationName.swift
+//  UtilityKit
+//
+//  Created by Dikran Teymur on 1.01.2024.
+//
+
+import Foundation
+
+public extension Notification.Name {
+    
+    static let setBagTotalNumber = Notification.Name("set_bag_total_number")
+    static let decreaseBagTotalNumber = Notification.Name("decrease_bag_total_number")
+    static let resetBagTotalNumber = Notification.Name("reset_bag_total_number")
+}
