d54928d5b54ab30f43a8704ab381d19cb3f35ff0
dikranteymur <mdteymur@gmail.com>
2024-01-02 20:09:41 +0300

    Add Extension+UIView

---
+++ ECommerceApp/DesignKit/Extensions/Extension+UIView.swift
@@ -0,0 +1,21 @@
+//
+//  Extension+UIView.swift
+//  DesignKit
+//
+//  Created by Dikran Teymur on 2.01.2024.
+//
+
+import UIKit
+
+public extension UIView {
+    
+    func addShadow(_ color: UIColor = .colorBlack,
+                   radius: CGFloat = 10,
+                   offset: CGSize = CGSize(width: 0, height: 4), 
+                   opacity: Float = 0.8) {
+        layer.shadowColor = color.cgColor
+        layer.shadowRadius = radius
+        layer.shadowOffset = offset
+        layer.shadowOpacity = opacity
+    }
+}
