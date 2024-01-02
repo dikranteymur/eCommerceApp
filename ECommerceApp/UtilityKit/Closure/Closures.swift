28159d4d89c70844a35ad1470fb8970d2c17b845
dikranteymur <mdteymur@gmail.com>
2024-01-02 03:24:15 +0300

    Add new typealias into closures

--- ECommerceApp/UtilityKit/Closure/Closures.swift
+++ ECommerceApp/UtilityKit/Closure/Closures.swift
@@ -7,5 +7,7 @@
 
 public typealias VoidClosure = () -> Void
 public typealias BoolClosure = (Bool) -> Void
+public typealias IntClosure = (Int) -> Void
+public typealias StringClosure = (String) -> Void
 public typealias ArrayClosure<T: Any> = ([T]) -> Void
 public typealias AnyClosure<T: Any> = (T) -> Void
